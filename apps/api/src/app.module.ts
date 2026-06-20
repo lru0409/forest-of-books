1f28b32ef7fde1383a37de5896578f85221eeb0c
lru0409 <lru040409@gmail.com>
2026-06-20 12:48:48 +0900

    feat(api): add daily cron to remove orphan profile images

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,12 @@ import { AuthModule } from './auth/auth.module';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UploadModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    AuthModule,
+    UploadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
